f57340cb809af0817287af6d87bd052d0a101e6b
rajmakvana143 <rajmakvana143e@gmail.com>
2024-09-08 09:58:24 +0530

    final commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true})); 
 
 // Genrative AI key = AIzaSyDdLz6glKBRK2-CxP441V7kp70nTea4bYo
 
@@ -21,10 +21,10 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 // const prompt = "what is javaScript ?";
 
 
-const getresult = async () => {
+const getresult = async (prompt) => {
     try {
         const response = await model.generateContent(prompt);
-        console.log(response.response.text());
+        return response.response.text();
     } catch (error) {
         console.error(error);
     }
@@ -36,14 +36,24 @@ app.listen(port , () => {
     console.log(`server listing on ${port}`);
 });
 
-app.get('/genrativeAI' , (req, res) => {
+
+app.get('/genrativeAI' , (req , res) => {
+    res.render('index.ejs');
+})
+
+app.post('/genrativeAI' , async (req, res) => {
     const {question} = req.body;
     console.log(question);
-    res.send("hello");
+    const genrated = await getresult(question);
+    // console.log(genrated);
+    const para =  spliting(genrated);
+    res.render('index.ejs' , {para,question});
 }); 
 
-
-
+const spliting = (data) => {
+    const para = data.split('**');
+    return para;
+}
 
 
 
